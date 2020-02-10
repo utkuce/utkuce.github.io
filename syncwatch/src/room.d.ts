ac0d5cb34d9b5d46e8293fb69b38b35c77c4f3fb
utku <utkuce@gmail.com>
2020-02-10 15:17:15 -0500

    Add syncwatch folder

---
+++ syncwatch/src/room.d.ts
@@ -0,0 +1,5 @@
+import 'firebase/auth';
+import 'firebase/database';
+export declare function create(): string;
+export declare function sendData(data: any): void;
+export declare function join(rId: string): void;
---
+++ syncwatch/src/video.d.ts
@@ -0,0 +1,5 @@
+import 'videojs-youtube';
+export declare function setPause(value: boolean): void;
+export declare function seekTo(seconds: number): void;
+export declare function sourceInput(): void;
+export declare function setSource(sourceURL: string): void;
