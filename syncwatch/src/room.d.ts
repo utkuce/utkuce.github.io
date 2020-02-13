d688f36668630baf2e1a06b4ff9bec222cb5672e
utku <utkuce@gmail.com>
2020-02-13 11:34:26 -0500

    Update syncwatch

--- syncwatch/src/room.d.ts
+++ syncwatch/src/room.d.ts
@@ -1,5 +1,5 @@
 import 'firebase/auth';
 import 'firebase/database';
-export declare function create(): string;
+export declare function create(): void;
 export declare function sendData(data: any): void;
 export declare function join(rId: string): void;
