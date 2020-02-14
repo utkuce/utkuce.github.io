ff6b8b0761ea11c58a1b606e3787b166564cae2f
utku <utkuce@gmail.com>
2020-02-14 16:49:13 -0500

    Move to syncwatch-web:gh-pages

--- syncwatch/src/video.d.ts
+++
@@ -1,5 +0,0 @@
-import 'videojs-youtube';
-export declare function setPause(value: boolean): void;
-export declare function seekTo(seconds: number): void;
-export declare function sourceInput(): void;
-export declare function setSource(sourceURL: string): void;
